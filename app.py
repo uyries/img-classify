0ebca19eb56966fd3742d00e082cb7d543682dea
jay <167955116+uyries@users.noreply.github.com>
2025-04-20 15:34:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# -- coding: utf-8 --
+"""
+Created on Sun Apr 20 13:52:09 2025
+
+@author: LAB
+"""
 import streamlit as st
 from tensorflow.keras.applications.mobilenet_v2 import decode_predictions, preprocess_input
 from tensorflow.keras.preprocessing import image
@@ -5,33 +11,26 @@ import numpy as np
 from PIL import Image
 import pickle
 
-# Load model
-with open('model.pkl', 'rb') as f:
+with open ('model.pkl', 'rb') as f:
     model = pickle.load(f)
+    
+st.title("Image Classification with MobileNetV2 by Phitchayatida")
 
+upload_file = st.file_uploader("Upload image:", type=["jpg", "jpeg", "png"])
 
-# App title
-st.title("🖼️ Image Classification with MobileNetV2")
-
-# File uploader
-uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
-
-if uploaded_file is not None:
-    # Display image
-    img = Image.open(uploaded_file)
-    st.image(img, caption="Uploaded Image")
-
-    # Preprocess the image
+if upload_file is not None:
+    img = Image.open(upload_file)
+    st.image(img, caption="Upload image")
+    
     img = img.resize((224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
-
+    
     # Prediction
     preds = model.predict(x)
     top_preds = decode_predictions(preds, top=3)[0]
-
-    # Display predictions
-    st.subheader("Predictions:")
+    
+    st.subheader("Prediction:")
     for i, pred in enumerate(top_preds):
         st.write(f"{i+1}. **{pred[1]}** — {round(pred[2]*100, 2)}%")
